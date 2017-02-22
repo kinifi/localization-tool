275ce306b1edd722d1c0abec04e356072e9d54ee
kinifi <chrisfigueroa16@gmail.com>
2017-02-22 14:12:33 -0800

    organized and made manager prefabs

---
+++ Assets/Language-Tool/Test Assets/scripts/loadscene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadscene : MonoBehaviour {
+
+    public string levelName;
+
+	
+	public void LoadLevel ()
+    {
+        SceneManager.LoadScene(levelName);	
+	}
+	
+}
